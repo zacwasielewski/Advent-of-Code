c9c5575bc20360dc0c2eea22e8f40527f55b5703
Zac Wasielewski <zac@wasielewski.org>
2015-12-04 22:46:28 -0500

    Refactor day 3

--- day3.rb
+++ day3.rb
@@ -1,39 +1,41 @@
 #!/usr/bin/env ruby
 
-def move_from(house, direction)
-  x, y = house
-  case direction
-  when '^'
-    house = [ x, y+1 ]
-  when 'v'
-    house = [ x, y-1 ]
-  when '>'
-    house = [ x+1, y ]
-  when '<'
-    house = [ x-1, y ]
+class Day3
+  def initialize(input)
+    @input = input.strip
   end
-  house
-end
 
-def deliver_presents(input)
-  house = [0, 0]
-  houses = [ house ]
-  input.strip.each_char { |direction|
-    house = move_from(house, direction)
-    houses << house
-  }
-  houses
-end
+  def unique_houses_visited_by_santa
+    deliver_presents(@input).uniq.size
+  end
 
-def unique_houses_visited_by_santa(input)
-  deliver_presents(input).uniq.size
-end
+  def unique_houses_visited_by_santa_and_robosanta
+    even_indexes = (0..@input.length-1).select(&:even?)
+    odd_indexes  = (0..@input.length-1).select(&:odd?)
 
-def unique_houses_visited_by_santa_and_robosanta(input)
-  even_indexes = (0..input.length-1).select(&:even?)
-  odd_indexes  = (0..input.length-1).select(&:odd?)
-  santa     = input.chars.values_at( *even_indexes ).join
-  robosanta = input.chars.values_at( *odd_indexes  ).join
+    santa     = @input.chars.values_at( *even_indexes ).join
+    robosanta = @input.chars.values_at( *odd_indexes  ).join
 
-  (deliver_presents(santa) + deliver_presents(robosanta)).uniq.size
+    (deliver_presents(santa) + deliver_presents(robosanta)).uniq.size
+  end
+
+  private
+
+  def deliver_presents(input)
+    start_house = [0, 0]
+    input.split('').reduce([start_house]) do |houses, direction|
+      houses << move_from(houses.last, direction)
+    end
+  end
+
+  def move_from(house, direction)
+    x, y = house
+    { '^' => -> { y += 1 },
+      'v' => -> { y -= 1 },
+      '>' => -> { x += 1 },
+      '<' => -> { x -= 1 }
+    }[direction].call
+    [x, y]
+  end
+
 end
--- test/test_day3.rb
+++ test/test_day3.rb
@@ -4,21 +4,20 @@ require './day3.rb'
 
 class TestDay3 < Minitest::Test
   def setup
-    @input_file = "./input/input_day3.txt"
-    @input = File.open(@input_file).read
+    @input = File.open("./input/input_day3.txt").read
   end
 
   def test_part_1
-    assert_equal 2, unique_houses_visited_by_santa('>')
-    assert_equal 4, unique_houses_visited_by_santa('^>v<')
-    assert_equal 2, unique_houses_visited_by_santa('^v^v^v^v^v')
-    assert_equal 2081, unique_houses_visited_by_santa(@input)
+    assert_equal 2, Day3.new('>').unique_houses_visited_by_santa
+    assert_equal 4, Day3.new('^>v<').unique_houses_visited_by_santa
+    assert_equal 2, Day3.new('^v^v^v^v^v').unique_houses_visited_by_santa
+    assert_equal 2081, Day3.new(@input).unique_houses_visited_by_santa
   end
 
   def test_part_2
-    assert_equal 3, unique_houses_visited_by_santa_and_robosanta('^v')
-    assert_equal 3, unique_houses_visited_by_santa_and_robosanta('^>v<')
-    assert_equal 11, unique_houses_visited_by_santa_and_robosanta('^v^v^v^v^v')
-    assert_equal 2341, unique_houses_visited_by_santa_and_robosanta(@input)
+    assert_equal 3, Day3.new('^v').unique_houses_visited_by_santa_and_robosanta
+    assert_equal 3, Day3.new('^>v<').unique_houses_visited_by_santa_and_robosanta
+    assert_equal 11, Day3.new('^v^v^v^v^v').unique_houses_visited_by_santa_and_robosanta
+    assert_equal 2341, Day3.new(@input).unique_houses_visited_by_santa_and_robosanta
   end
 end
