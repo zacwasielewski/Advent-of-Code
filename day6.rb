96ad7eb69199715a6f0919db73e6081bba0e3d3b
Zac Wasielewski <zac@wasielewski.org>
2015-12-06 12:44:38 -0500

    Working Day 6 solutions

--- day6.rb
+++ day6.rb
@@ -1,10 +1,104 @@
 #!/usr/bin/env ruby
 
+require 'matrix'
+
 module Day6
   def self.solve_part_1(input)
-    input.strip.split("\n").each do |str|
+    @lights = Lights.new
+    input.strip.split("\n").each {|str| @lights.change(str) }
+    @lights.on_count
+  end
 
+  def self.solve_part_2(input)
+    @lights = DimmableLights.new
+    input.strip.split("\n").each {|str| @lights.change(str) }
+    @lights.total_brightness
+  end
+end
 
+class Lights
+  def initialize
+    @grid = Matrix.build(1000, 1000){ false }
+  end
+
+  def change(str)
+    command, startxy, endxy = parse_instructions str
+
+    @grid.each_with_index do |e, row, col|
+      next if row < startxy[0] || row > endxy[0]
+      next if col < startxy[1] || col > endxy[1]
+      case command
+      when 'on'
+        @grid.send(:[]=, row, col, true)
+      when 'off'
+        @grid.send(:[]=, row, col, false)
+      when 'toggle'
+        @grid.send(:[]=, row, col, !e)
+      end
     end
+
+    self
+  end
+
+  def on_count
+    @grid.map{|e| { :true => 1, :false => 0 }[e.to_s.to_sym] }.reduce(:+)
+  end
+
+  private
+
+  def parse_instructions(str)
+    str.gsub!('turn ','') # drop some cruft
+    command, startxy, _, endxy = str.split ' '
+    [ command,
+      startxy.split(',').map(&:to_i),
+      endxy.split(',').map(&:to_i) ]
+  end
+end
+
+
+class DimmableLights
+  def initialize
+    @grid = Matrix.build(1000, 1000){ 0 }
+  end
+
+  def change(str)
+    command, startxy, endxy = parse_instructions str
+
+    @grid.each_with_index do |e, row, col|
+      next if row < startxy[0] || row > endxy[0]
+      next if col < startxy[1] || col > endxy[1]
+      commands = {
+        'on'     => -> { brighten(e) },
+        'off'    => -> { dim(e) },
+        'toggle' => -> { brighten(e, 2) }
+      }
+      @grid.send(:[]=, row, col, commands[command].call)
+    end
+
+    self
+  end
+
+  def dim(brightness, amount = 1)
+    brightness -= amount
+    return 0 if brightness < 0
+    brightness
+  end
+
+  def brighten(brightness, amount = 1)
+    brightness += amount
+  end
+
+  def total_brightness
+    @grid.reduce(:+)
+  end
+
+  private
+
+  def parse_instructions(str)
+    str.gsub!('turn ','') # drop some cruft
+    command, startxy, _, endxy = str.split ' '
+    [ command,
+      startxy.split(',').map(&:to_i),
+      endxy.split(',').map(&:to_i) ]
   end
 end
--- test/test_day6.rb
+++ test/test_day6.rb
@@ -7,10 +7,37 @@ class TestDay6 < Minitest::Test
   end
 
   def test_part_1
-    assert_equal _, Day6.solve_part_1(@input)
+    assert_equal 400410, Day6.solve_part_1(@input)
   end
 
   def test_part_2
-    #assert_equal 51, Day6.solve_part_2(@input)
+    assert_equal 15343601, Day6.solve_part_2(@input)
   end
+end
+
+class TestLights < Minitest::Test
+  def setup
+    @lights = Lights.new
+  end
+
+  def test_sequence
+    assert_equal 0, @lights.on_count
+    assert_equal 1_000_000, @lights.change('turn on 0,0 through 999,999').on_count
+    assert_equal 999000, @lights.change('toggle 0,0 through 999,0').on_count
+    assert_equal 998996, @lights.change('turn off 499,499 through 500,500').on_count
+  end
+
+end
+
+class TestDimmableLights < Minitest::Test
+  def setup
+    @lights = DimmableLights.new
+  end
+
+  def test_sequence
+    assert_equal 0, @lights.total_brightness
+    assert_equal 1, @lights.change('turn on 0,0 through 0,0').total_brightness
+    assert_equal 2000001, @lights.change('toggle 0,0 through 999,999').total_brightness
+  end
+
 end
